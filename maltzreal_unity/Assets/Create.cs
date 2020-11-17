2cf01c29ee3e6d2e3601790789d5db2eb803b7c6
anaammostafiz <anaam.mostafiz@gmail.com>
2020-11-16 19:04:05 -0500

    added base unity project

---
+++ maltzreal_unity/Assets/Create.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class create : MonoBehaviour
+{
+
+    public GameObject Duplicate;
+    void OnMouseDown()
+    {
+        GameObject Clone = Instantiate(Duplicate);
+        Clone.transform.position = new Vector3(3, 3, -2);
+
+    }
+}
